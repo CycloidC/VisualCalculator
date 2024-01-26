17d845a593fb07e5e06ff2cc58e6454b8e32c931
cycloidc <122423837+CycloidC@users.noreply.github.com>
2024-01-25 20:24:58 -0600

    Add HTML, CSS, and initial js

---
+++ main.js
@@ -0,0 +1,22 @@
+function addition(a, b) {
+    return a + b;
+};
+
+function subtraction(a, b) {
+    return a - b;
+};
+
+function multiplication(a, b) {
+    return a * b;
+};
+
+function division(a, b) {
+    return a / b;
+};
+
+function operate(num1, operator, num2) {
+    //switch case to call each function for
+    //respective operator. Use num1 and num2 as
+    //a and b in each function.
+}
+
