4f93439d8d065e42087f3474614df9e7e0fadb74
cycloidc <122423837+CycloidC@users.noreply.github.com>
2024-01-28 20:44:29 -0600

    Add js functions for buttons

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
+let displayString = "";
+let inputString = "";
+let inputArray = [];
+const numButtons = document.querySelectorAll(".num-buttons");
+const funcButtons = document.querySelectorAll(".func-buttons");
+const displayDiv = document.querySelector(".display");
+const clearButton = document.querySelector("#clear-button");
+const equalButton = document.querySelector("#equals-button");
+
+for (let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener('click', function() {
+        displayString = displayString + numButtons[i].innerText;
+        inputString = inputString + numButtons[i].innerText;
+        displayDiv.innerHTML = displayString;
+    });
+};
+
+for (let i = 0; i < funcButtons.length; i++) {
+    funcButtons[i].addEventListener('click', function() {
+        inputArray.push(inputString);
+        inputArray.push(funcButtons[i].innerText);
+        displayString = displayString + funcButtons[i].innerText;
+        displayDiv.innerHTML = displayString;
+        inputString = "";
+    });
+};
+
+clearButton.addEventListener('click', function() {
+    displayString = "";
+    inputString = "";
+    inputArray = [];
+    displayDiv.innerHTML = "...";
+});
+
+equalButton.addEventListener('click', function() {
+    inputArray.push(inputString);
+    while (inputArray.length > 1) {
+        var newValue = operate(inputArray[0], inputArray[1], inputArray[2])
+        inputArray.shift();
+        inputArray.shift();
+        inputArray.shift();
+        inputArray.unshift(newValue);
+    };
+    displayString = inputArray[0];
+    inputString = inputArray[0];
+    displayDiv.innerHTML = displayString;
+});
+
 function addition(a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 };
 
 function subtraction(a, b) {
-    return a - b;
+    return parseFloat(a) - parseFloat(b);
 };
 
 function multiplication(a, b) {
@@ -15,11 +63,15 @@ function division(a, b) {
 };
 
 function operate(num1, operator, num2) {
-    //switch case to call each function for
-    //respective operator. Use num1 and num2 as
-    //a and b in each function.
-}
-
-//notes for operator button functions:
-//button must add stored value to array, add the operator in next spot in array, then clear the stored value for the next one
+    switch (operator) {
+        case "+" :
+            return addition(num1, num2);
+        case "-" :
+            return subtraction(num1, num2);
+        case "X" :
+            return multiplication(num1, num2);
+        case "/" :
+            return division(num1, num2);
+    };
+};
 
